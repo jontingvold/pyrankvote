293e160d842581fe087efcf0be1f98275dabe2ba
Jon Tingvold <jon.tingvold@gmail.com>
2019-09-14 14:14:48 +0200

    Bugfix: tabulate not added to setup.py

--- pyrankvote/__init__.py
+++ pyrankvote/__init__.py
@@ -1,6 +1,6 @@
 from pyrankvote import single_seat_ranking_methods, multiple_seat_ranking_methods
 
-__version__ = '1.0.1'
+__version__ = '1.0.4'
 
 __all__ = [
     'single_seat_ranking_methods',
--- setup.py
+++ setup.py
@@ -2,10 +2,16 @@ import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+    
+import unittest
+def my_test_suite():
+    test_loader = unittest.TestLoader()
+    test_suite = test_loader.discover('tests', pattern='test_*.py')
+    return test_suite
 
 setuptools.setup(
     name='pyrankvote',
-    version='1.0.1',
+    version='1.0.4',
     author='Jon Tingvold',
     author_email='jon.tingvold@gmail.com',
     description='PyRankVote is a python library for different ranked voting methods, '
@@ -13,14 +19,21 @@ setuptools.setup(
                 'created by Jon Tingvold.',
     license='MIT',
     url='https://github.com/jontingvold/pyrankvote',
-
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    setup_requires=[],
-    packages=setuptools.find_packages(exclude=['tests']),
+    
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    
+    setup_requires=[],
+    install_requires=[
+        'tabulate',
+    ],
+    
+    packages=setuptools.find_packages(exclude=['tests']),
+    
+    test_suite="setup.my_test_suite",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )
