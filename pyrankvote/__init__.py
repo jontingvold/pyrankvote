23c9513f68580b2aeea0b5a40bdf5672fc05b46c
Jon Tingvold <jon.tingvold@gmail.com>
2019-09-14 17:54:49 +0200

    Increase version number to v1.0.5

--- pyrankvote/__init__.py
+++ pyrankvote/__init__.py
@@ -2,7 +2,7 @@ from pyrankvote.models import Candidate, Ballot
 from pyrankvote.single_seat_ranking_methods import instant_runoff_voting
 from pyrankvote.multiple_seat_ranking_methods import single_transferable_vote, preferential_block_voting
 
-__version__ = '1.0.4'
+__version__ = '1.0.7'
 
 __all__ = [
     'Candidate',
