04c6eecb771b5b91b9d27f8b7ed2d3d1046eabcc
Jon Tingvold <jon.tingvold@gmail.com>
2019-09-15 02:32:32 +0200

    Increase version to v1.0.8

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def my_test_suite():
 
 setuptools.setup(
     name='pyrankvote',
-    version='1.0.7',
+    version='1.0.8',
     author='Jon Tingvold',
     author_email='jon.tingvold@gmail.com',
     description='PyRankVote is a python library for different ranked voting methods, '
