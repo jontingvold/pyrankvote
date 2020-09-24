297bcf157aaadab92d96278b58a6e3c9ea50f881
Jon Tingvold <jon.tingvold@gmail.com>
2020-09-24 21:15:23 +0200

    Set version number to 2.0.2

--- pyrankvote/__init__.py
+++ pyrankvote/__init__.py
@@ -2,7 +2,7 @@ from pyrankvote.models import Candidate, Ballot
 from pyrankvote.single_seat_ranking_methods import instant_runoff_voting
 from pyrankvote.multiple_seat_ranking_methods import single_transferable_vote, preferential_block_voting
 
-__version__ = '2.0.1'
+__version__ = '2.0.2'
 
 __all__ = [
     'Candidate',
