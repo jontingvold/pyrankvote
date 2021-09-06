8ea71300df21104baf86372c6c02b95971967745
Julius <julius.simonelli@gmail.com>
2021-09-05 19:33:42 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,34 +2,32 @@ import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
-    
+
 import unittest
+
+
 def my_test_suite():
     test_loader = unittest.TestLoader()
-    test_suite = test_loader.discover('tests', pattern='test_*.py')
+    test_suite = test_loader.discover("tests", pattern="test_*.py")
     return test_suite
 
+
 setuptools.setup(
-    name='votesim',
-    version='2.0.2',
-    description='VoteSim is a python library for different ranked voting methods, '
-                'like instant-runoff voting, single transferable vote and preferential block voting, '
-    license='MIT',
-    url='https://github.com/jss367/votesim',
-    
+    name="votesim",
+    version="2.0.2",
+    description="VoteSim is a python library for different voting methods",
+    license="MIT",
+    url="https://github.com/jss367/votesim",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    
     setup_requires=[],
     install_requires=[
-        'tabulate',
+        "tabulate",
     ],
-    
-    packages=setuptools.find_packages(exclude=['tests', 'test_data']),
-    
+    packages=setuptools.find_packages(exclude=["tests", "test_data"]),
     test_suite="setup.my_test_suite",
     long_description=long_description,
     long_description_content_type="text/markdown",
