2ad32472dbb704ecad1e3aa77d6bfc0d05083dbe
Jon Tingvold <jon.tingvold@gmail.com>
2022-06-13 22:21:02 +0200

    Update version to 2.0.5

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def my_test_suite():
 
 setuptools.setup(
     name='pyrankvote',
-    version='2.0.4',
+    version='2.0.5',
     author='Jon Tingvold',
     author_email='jon.tingvold@gmail.com',
     description='PyRankVote is a python library for different ranked voting methods, '
