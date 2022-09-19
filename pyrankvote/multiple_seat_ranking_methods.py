b0e2f37977452a997db8c94ac303092a700f3f02
Glenn <glenn.lee.23@seoulforeign.org>
2022-09-19 21:28:28 +0900

    fixing droop quota

--- pyrankvote/multiple_seat_ranking_methods.py
+++ pyrankvote/multiple_seat_ranking_methods.py
@@ -157,7 +157,7 @@ def single_transferable_vote(
     election_results = ElectionResults()
 
     voters, seats = manager.get_number_of_non_exhausted_ballots(), number_of_seats
-    votes_needed_to_win: float = voters / float((seats + 1))  # Drop quota
+    votes_needed_to_win: float = round((voters / (seats + 1))+1)  # Editied droop quota
 
     # Remove worst candidate until same number of candidates left as electable
     # While it is more candidates left than electable
